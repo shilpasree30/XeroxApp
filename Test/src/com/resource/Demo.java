46c31665c3b9528172e7413f27a30ac832677dc0
shilpasree30 <sshilpasree93@gmail.com>
2019-09-21 16:46:22 +0530

    New class in resource folder

---
+++ Test/src/com/resource/Demo.java
@@ -0,0 +1,11 @@
+package com.resource;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Main Started");
+		int a = 10;
+		System.out.println("Value of a =" +a);
+		System.out.println("Main Ended");
+	}
+}
